e8e57b242ad6016fc1c20ce9077a47f970ec027d
Alan Justino <alan.justino@yahoo.com.br>
2019-04-01 14:19:38 -0300

    Initial structure

---
+++ nondjango/__init__.py
@@ -0,0 +1,7 @@
+"""
+nondjango-storages - Because the API is great but dependency on Django is not.
+"""
+__version__ = '0.1.0'
+
+# Allows `nondjango.something` to be on another package in the future
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
